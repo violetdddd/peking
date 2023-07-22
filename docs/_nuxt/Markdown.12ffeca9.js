91c52f8b58c9ad7a2d163d9eb431e0c679db0fc2
Junduo Zheng <1253314323@qq.com>
2023-07-22 13:43:38 +0800

    src

--- docs/_nuxt/Markdown.12ffeca9.js
+++
@@ -1,1 +0,0 @@
-import r from"./ContentSlot.fec08241.js";import{g as o,B as u,s as f,O as c}from"./entry.07f755ed.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:s}=u(),a=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:s,tags:a,between:n,parent:e}}});export{i as default};
