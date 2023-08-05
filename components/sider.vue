061004f3e006ffca3f4bd3e79e5d9a8b1692676f
Mike-Sagiri <dangkuchangge@gmail.com>
2023-08-05 15:33:51 +0800

    solve sidebar smooth scrolling; cancel plans to add animation of scrolling.

---
+++ components/sider.vue
@@ -0,0 +1,32 @@
+<template>
+    
+    <div class="scroll-smooth font-medium translate-y-[-50%] mt-[200px] mr-[32px] mb-[0px] ml-[4vw] uppercase sticky top-[50%] min-w-[220px] max-w-[350px] float-left ">
+        <h1 class="font-light">{{ page.title }}</h1>
+        <ul class="font-extralight">
+            
+            <li v-for="e in toc.links" >
+                <div class=" flex cursor-pointer items-center mt-6">
+                <div class="mr-[8px] border-solid border max-w-[15px] max-h-[15px] h-[10vw] w-[10vh] rounded-full border-blue-500"></div>
+                <NuxtLink class=" text-lg" :href="'#' + e.id">{{ e.text }}</NuxtLink></div>
+                <ul>
+                    <li v-for="c in e.children">
+                        <div class="flex cursor-pointer items-center mt-4">
+                        <div class="ml-[16px] mr-[8px] border-solid border max-w-[15px] max-h-[15px] h-[8vw] w-[8vh] rounded-full border-blue-500"></div>
+                        <NuxtLink :href="'#' + c.id">{{ c.text }}</NuxtLink></div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const { toc, page } = useContent();
+
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex items-start justify-evenly relative ">
+  <div class="flex items-start justify-evenly relative scroll-smooth">
     <!-- <AppSideBar /> -->
     <Sider />
     <div
-      class=" my-8 flex-grow max-w-2xl px-4 py-10 bg-white sm:px-8 sm:shadow ring-1 ring-gray-200 sm:rounded-lg"
+      class="scroll-smooth font-light my-8 flex-grow max-w-2xl px-4 py-10 bg-white sm:px-8 sm:shadow ring-1 ring-gray-200 sm:rounded-lg"
     >
-      <main class="max-w-none">
+      <main class="max-w-none scroll-smooth">
         
         <ContentDoc>
           <template #empty>
@@ -21,3 +21,12 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+
+</style>
