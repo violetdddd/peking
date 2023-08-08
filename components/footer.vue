0edf094f01df7789db77e24abcc0abaa6b6edd6b
Mike-Sagiri <dangkuchangge@gmail.com>
2023-08-08 15:11:47 +0800

    try to solve navigation problem

--- components/footer.vue
+++ components/footer.vue
@@ -21,6 +21,7 @@
             </div>
             <hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-4" />
             <div class=" grid grid-cols-2 gap-8 px-2 py-4 lg:py-6 md:grid-cols-5">
+                <ContentNavigation v-slot="{ navigation }">
                 <div v-for="(item, index) in bigTitle">
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{{ item }}</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium text-xs">
@@ -31,6 +32,7 @@
                         </li>
                     </ul>
                 </div>
+            </ContentNavigation>
             </div>
             <hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-4" />
             <div class="">
