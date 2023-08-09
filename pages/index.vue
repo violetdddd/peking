efd954884a2fe168ca9e155797e626a2505eec9a
Mike-Sagiri <dangkuchangge@gmail.com>
2023-08-09 14:02:44 +0800

    change landing page

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,11 @@
         <h1
           class="text-center w-full pt-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-9xl font-bold"
         >
-          Hello world!
+          Comming Soon!
         </h1>
       
         <p class="text-center max-w-sm mx-auto pt-20">
-          This will be a fancy website landing page
+          This is a draft version of igem Peking wiki.
         </p>
     </div>
 </template>
