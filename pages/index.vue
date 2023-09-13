be853c2bff8985ff8b79c7337b50fab8d950a822
Mike-Sagiri <dangkuchangge@gmail.com>
2023-09-13 20:17:17 +0800

    address index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-
     <div>
-      <Index-sider></Index-sider>
+      <IndexSider />
       <Title>Home</Title>
         <h1
           class="text-center w-full pt-20 pb-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-9xl font-bold"
